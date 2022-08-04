fdabe2b7bcd6465b7f2eed5b8d7a16aa921ebdea
Jony <jonyaleon@gmail.com>
2022-08-05 01:07:11 +0600

    initial commit

---
+++ src/main/java/com/excellentnomad/spring/security/postgresql/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.excellentnomad.spring.security.postgresql.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.excellentnomad.spring.security.postgresql.models.ERole;
+import com.excellentnomad.spring.security.postgresql.models.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(ERole name);
+}
